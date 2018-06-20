91ce9426eb1a994f228ed1c1bf4ed70c42985617
shalu <shaluchauhan024@gmail.com>
2018-06-19 23:31:07 -0700

    my modified file

--- class9/ABC.java
+++ class9/ABC.java
@@ -5,17 +5,6 @@ public static void main(String[] args)throws IOException
 {
 File f=new File("d:\\JavaCode\\ABC.txt");
 FileInputStream fis=new FileInputStream (f);
-FileOutputStream fos= new FileOutputStream(f);
-
-
-String text="I Started Learning Java";
-byte[] b=text.getBytes();
-for(byte b2:b)
-{
-fos.write(b2);
-}
-fos.flush();
-fos.close();
 
 int c;
 while((c=fis.read())!= -1){
